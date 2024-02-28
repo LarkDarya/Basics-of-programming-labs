e862b2471b48e2d7410aa1189b0ed230d3a38898
LarkDarya <161061909+LarkDarya@users.noreply.github.com>
2024-02-28 16:40:14 +0300

    Update main_vectorVoid.c

--- libs/data_structures/vector/main_vectorVoid.c
+++ libs/data_structures/vector/main_vectorVoid.c
@@ -4,17 +4,22 @@
 int main() {
     size_t n;
     scanf("%zd", &n);
+    
     vectorVoid v = createVectorV(0, sizeof(int));
+    
     for (int i = 0; i < n; i++) {
         int x;
         scanf("%d", &x);
         pushBackV(&v, &x);
     }
+    
     for (int i = 0; i < n; i++) {
         int x;
+        
         getVectorValueV(&v, i, &x);
         printf("%d ", x);
     }
+    
     return 0;
 }
 
