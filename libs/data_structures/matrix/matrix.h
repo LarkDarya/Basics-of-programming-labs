589faeff00d3d34d21f32974f614811969cd15c4
LarkDarya <darya.zhavoronkova25052005@gmail.com>
2024-03-10 14:19:46 +0300

    implementation of functions for working with dynamic memory

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -12,4 +12,19 @@ typedef struct position {
     int colIndex;
 } position;
 
+<<<<<<< HEAD
+=======
+//размещает в динамической памяти матрицу размером nRows на nCols
+matrix getMemMatrix(int nRows, int nCols);
+
+//размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+//освобождает память, выделенную под хранение матрицы m
+void freeMemMatrix(matrix *m);
+
+//освобождает память, выделенную под хранение массива ms из nMatrices матриц
+void freeMemMatrices(matrix *ms, int nMatrices);
+
+>>>>>>> 4316811 (implementation of functions for working with dynamic memory)
 #endif //BASICS_OF_PROGRAMMING_LABS_MATRIX_H
