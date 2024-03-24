19e1f172d7eeab91c9c1ee6286019cbece0097ca
LarkDarya <darya.zhavoronkova25052005@gmail.com>
2024-03-24 12:36:05 +0300

    fourth task

--- libs/data_structures/matrix/matrix_task.h
+++ libs/data_structures/matrix/matrix_task.h
@@ -10,7 +10,30 @@ void firstTask(matrix *m);
 //Задание 2: упорядочивает строки матрицы по неубыванию наибольших элементов строк.
 void secondTask(matrix m);
 
-//Задание 3: упорядочивает столбцы матрицы по неубыванию минимальных элементов столбцов
+//Задание 3: упорядочивает столбцы матрицы по неубыванию минимальных элементов столбцов.
 void thirdTask(matrix m);
+
+//Задание 4: заменяет квадратную матрицу её квадратом.
+matrix fourthTask(matrix m);
+
+
+
+// функция-критерий для сравнения строк по максимальному элементу
+int getMax(int* a, int n);
+
+//сортирует строки по неубывнию наибольших элементов строк
+void sortRowsByMaxElement(matrix m);
+
+// функция-критерий для сравнения строк по минимальному элементу
+int getMin(int *a, int n);
+
+//упорядочивает столбцы матрицы по неубыванию минимальных элементов столбцов
+void sortColsByMinElemnt(matrix m);
+
+//умножает матрицы
+matrix mulMatrices(matrix m1, matrix m2);
+
+//возвращает квадрат матрицы, если она симметрична
+matrix getSquareOfMatrixIfSymmetric(matrix m);
 
 #endif //BASICS_OF_PROGRAMMING_LABS_MATRIX_TASKS_H
