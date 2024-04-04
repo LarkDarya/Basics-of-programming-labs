bae1b3779fd46991d12712a0ca299cd5654610ae
MrLenoks <Mr.Lenoks@yandex.ru>
2024-04-04 20:27:48 +0300

    add task 2 (test 1)

--- libs/string/tasks/main_string_.c
+++ libs/string/tasks/main_string_.c
@@ -3,21 +3,30 @@
 #include <assert.h>
 
 void test_strlen_(){
-    char *testString1 = "Hello";
-    int len1 = strlen_(testString1);
+    char *test_string1 = "Hello";
+    int len1 = strlen_(test_string1);
     assert(len1 == 5);
 
-    char *testString2 = "   "; //три пробела
-    int len2 = strlen_(testString2);
+    char *test_string2 = "   "; //три пробела
+    int len2 = strlen_(test_string2);
     assert(len2 == 3);
 
-    char *testString3 = "()!""№;%:\е \0";
-    int len3 = strlen_(testString3);
+    char *test_string3 = "()!""№;%:\е \0";
+    int len3 = strlen_(test_string3);
     assert(len3 == 12);
 }
 
+void test_find(){
+    char test_string[] = "Space";
+    char *found = find(test_string, test_string + 6, 'S');
+    printf("%d", *found);
+    assert(*found == 83);
+}
+
+
 void test(){
     test_strlen_();
+    test_find();
 }
 
 int main(){
