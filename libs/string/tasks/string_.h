45f07b452a4e7a7753c8edeb2f56eb66a05e2051
MrLenoks <Mr.Lenoks@yandex.ru>
2024-04-04 21:30:31 +0300

    add task 4

--- libs/string/tasks/string_.h
+++ libs/string/tasks/string_.h
@@ -12,4 +12,8 @@ char* find(char *begin, char *end, int ch);
 //начиная с begin и заканчивая ноль-символом
 char* findNonSpace(char *begin);
 
+//возвращает указатель на первый справа символ, отличный от пробельных,
+//расположенный на ленте памяти, начиная с rbegin
+char* findNonSpaceReverse(char *rbegin, const char *rend);
+
 #endif //BASICS_OF_PROGRAMMING_LABS_STRING__H
